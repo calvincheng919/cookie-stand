0d25fa9dfeb60bd9c32177edcadeafa09ca4ca45
Calvin Cheng <calvincheng919@gmail.com>
2019-03-19 23:48:13 -0700

    created constructor

--- js/app.js
+++ js/app.js
@@ -2,6 +2,54 @@
 
 let hours = ['6am', '7am', '8am', '9am','10am', '11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
+
+let Store = function(location, min, max, avg){
+  this.location = location;
+  this.min_cust = min;
+  this.max_cust = max;
+  this.avg_cookie_sale = avg;
+  this.customers_per_hour = [];
+  this.cookies_sold_per_hour = [];
+};
+
+Store.prototype.calcCustomersPerHour = function(){
+  for(let i = 0 ; i < hours.length ; i++){ 
+    this.customers_per_hour.push(random(this.min_cust, this.max_cust));
+  }
+};
+Store.prototype.calcCookiesPerHour = function(){
+  this.calcCustomersPerHour();
+  for (let i = 0 ; i < this.customers_per_hour.length; i++){
+    this.cookies_sold_per_hour.push(Math.floor(this.customers_per_hour[i] * this.avg_cookie_sale));
+  }
+};
+Store.prototype.render = function(){
+  let section = document.getElementById('salesSection');
+  let h3 = document.createElement('h3');
+  let ul = document.createElement('ul');
+  this.calcCookiesPerHour();
+  
+  h3.textContent = this.location;
+  section.appendChild(h3);
+  section.appendChild(ul);
+  
+  for (let i = 0; i < hours.length; i++){
+    let li = document.createElement('li');
+    li.textContent = `${hours[i]}: ${this.cookies_sold_per_hour[i]} cookies`;
+    ul.appendChild(li);
+  }
+
+  let li = document.createElement('li');
+  li.textContent = `Total: ${this.cookies_sold_per_hour.reduce((sum, curr) => {return sum+curr;})} cookies`;
+  ul.appendChild(li);
+};
+
+let oldmill = new Store('Old Mill', 12, 45, 4.5);
+// oldmill.calcCookiesPerHour();
+// console.log(`${oldmill.location}: ${oldmill.cookies_sold_per_hour}`);
+oldmill.render();
+
+
 //Fist and Pike oject
 let first_n_pike = {
   location: '1st and Pike',
